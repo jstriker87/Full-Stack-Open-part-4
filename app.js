92ac2c2585761ddd7f73c3fa979b432f7f2a0ad0
James Bart <jbart1@gmail.com>
2024-09-29 21:43:41 +0100

    Completed exercises 4.1 and 4.2

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 const express = require('express')
 const config = require('./utils/config')
-const express = require('express')
 const app = express()
 const cors = require('cors')
-const notesRouter = require('./controllers/notes')
+const blogsRouter = require('./controllers/blogs')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
@@ -25,47 +24,11 @@ app.use(express.static('dist'))
 app.use(express.json())
 app.use(middleware.requestLogger)
 
-app.use('/api/notes', notesRouter)
+app.use('/api/blogs', blogsRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 
 module.exports = app
 
-//const blogSchema = new mongoose.Schema({
-//  title: String,
-//  author: String,
-//  url: String,
-//  likes: Number
-//})
-//
-//const Blog = mongoose.model('Blog', blogSchema)
-//
-//const mongoUrl = 'mongodb://localhost/bloglist'
-//mongoose.connect(mongoUrl)
-//
-//app.use(cors())
-//app.use(express.json())
-//
-//app.get('/api/blogs', (request, response) => {
-//  Blog
-//    .find({})
-//    .then(blogs => {
-//      response.json(blogs)
-//    })
-//})
-//
-//app.post('/api/blogs', (request, response) => {
-//  const blog = new Blog(request.body)
-//
-//  blog
-//    .save()
-//    .then(result => {
-//      response.status(201).json(result)
-//    })
-//})
-//
-//const PORT = 3001
-//app.listen(PORT, () => {
-//  console.log(`Server running on port ${PORT}`)
-//})
+
