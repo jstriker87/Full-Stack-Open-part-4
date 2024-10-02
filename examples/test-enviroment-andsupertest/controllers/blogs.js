6de9373ca9d3ec3797e120f7dd78c7f38abea0ab
James Bart <jbart1@gmail.com>
2024-10-02 23:07:05 +0100

    Working on part 4 examples

---
+++ examples/test-enviroment-andsupertest/controllers/blogs.js
@@ -0,0 +1,64 @@
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
+
+blogsRouter.get('/', (request, response) => {
+  Blog.find({}).then(blogs => {
+    response.json(blogs)
+  })
+})
+
+blogsRouter.get('/info', (request, response) => {
+    response.send('<h1>Hello World!</h1>')
+})
+
+blogsRouter.get('/:id', (request, response, next) => {
+  Blog.findById(request.params.id)
+    .then(blog => {
+      if (blog) {
+        response.json(blog)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+})
+
+blogsRouter.post('/', (request, response, next) => {
+  const body = request.body
+
+  const blog = new Blog({
+    name: body.name,
+    url: body.url,
+  })
+
+  blog.save()
+    .then(savedBlog => {
+      response.json(savedBlog)
+    })
+    .catch(error => next(error))
+})
+
+blogsRouter.delete('/:id', (request, response, next) => {
+  Blog.findByIdAndDelete(request.params.id)
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+blogsRouter.put('/:id', (request, response, next) => {
+  const body = request.body
+
+  const blog = {
+    name: body.name,
+    url: body.url,
+  }
+
+Blog.findByIdAndUpdate(request.params.id,blog, { new: true })
+    .then(updatedBlog => {
+      response.json(updatedBlog)
+    })
+    .catch(error => next(error))
+})
+
+module.exports = blogsRouter
