4f5e7c43573c86083af0ed81022b272daf2090a8
James Bart <jbart1@gmail.com>
2024-11-10 14:06:11 +0000

    Still working on exercise 4.15

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,60 +1,59 @@
-const blogsRouter = require('express').Router()
-const Blog = require('../models/blog')
-const User = require('../models/user')
+const blogsRouter = require("express").Router();
+const Blog = require("../models/blog");
+const User = require("../models/user");
 
-blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
-    response.json(blogs)
-  })
+blogsRouter.get("/", async (request, response) => {
+  const blogs = await Blog.find({});
+  response.json(blogs);
+});
 
-blogsRouter.get('/info', async  (request, response) => {
-    response.send('<h1>Hello World!</h1>')
-})
+blogsRouter.get("/info", async (request, response) => {
+  response.send("<h1>Hello World!</h1>");
+});
 
-blogsRouter.get('/:id', async (request, response, next) => {
-    const blog = await Blog.findById(request.params.id)
-    if (blog) {
-        response.json(blog)
-      } else {
-        response.status(404).end()
-      }
-})
+blogsRouter.get("/:id", async (request, response, next) => {
+  const blog = await Blog.findById(request.params.id);
+  if (blog) {
+    response.json(blog);
+  } else {
+    response.status(404).end();
+  }
+});
 
-
-blogsRouter.post('/', async (request, response, next) => {
-  const body = request.body
-  const user = await User.findById(body.userId)
+blogsRouter.post("/", async (request, response, next) => {
+  const body = request.body;
+  const user = await User.findById(body.userId);
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
     likes: body.likes || 0,
-    user: body.id
+    user: body.id,
+  });
+  const savedBlog = await blog.save();
+  console.log("SAVE" + savedBlog);
+  user.blogs = user.blogs.concat(savedBlog._id);
+  await user.save();
+  response.status(201).json(savedBlog);
+});
 
-  })
-    const savedBlog = await blog.save()
-    user.blogs = user.blogs.concat(savedBlog._id)
-    await user.save()
-    response.status(201).json(savedBlog)
-    })
+blogsRouter.delete("/:id", async (request, response, next) => {
+  await Blog.findByIdAndDelete(request.params.id);
+  response.status(204).end();
+});
 
-blogsRouter.delete('/:id', async (request, response, next) => {
-  await Blog.findByIdAndDelete(request.params.id)
-      response.status(204).end()
-})
-
-blogsRouter.put('/:id', (request, response, next) => {
-  const body = request.body
+blogsRouter.put("/:id", (request, response, next) => {
+  const body = request.body;
 
   const blog = {
-    likes: body.likes
-  }
+    likes: body.likes,
+  };
 
-Blog.findByIdAndUpdate(request.params.id,blog, { new: true })
-    .then(updatedBlog => {
-      response.json(updatedBlog)
+  Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+    .then((updatedBlog) => {
+      response.json(updatedBlog);
     })
-    .catch(error => next(error))
-})
+    .catch((error) => next(error));
+});
 
-module.exports = blogsRouter
+module.exports = blogsRouter;
--- examples/populate/controllers/notes.js
+++ examples/populate/controllers/notes.js
@@ -1,59 +1,56 @@
-const notesRouter = require('express').Router()
-const Note = require('../models/note')
-const User = require('../models/user')
+const notesRouter = require("express").Router();
+const Note = require("../models/note");
+const User = require("../models/user");
 
-notesRouter.get('/', async (request, response) => {
+notesRouter.get("/", async (request, response) => {
+  const notes = await Note.find({});
+  response.json(notes);
+});
 
-  const notes = await Note.find({})
-    response.json(notes)
-  })
+notesRouter.get("/:id", async (request, response, next) => {
+  const note = await Note.findById(request.params.id);
+  if (note) {
+    response.json(note);
+  } else {
+    response.status(404).end();
+  }
+});
 
+notesRouter.post("/", async (request, response, next) => {
+  const body = request.body;
 
-notesRouter.get('/:id', async (request, response, next) => {
-    const note = await Note.findById(request.params.id)
-    if (note) {
-      response.json(note)
-    } else {
-      response.status(404).end()
-    }
-})
-
-notesRouter.post('/', async (request, response, next) => {
-  const body = request.body
- 
-  const user = await User.findById(body.userId)
+  const user = await User.findById(body.userId);
 
   const note = new Note({
     content: body.content,
     important: body.important || false,
-    user: user.id
-  })
-    const savedNote = await note.save()
-    user.notes = user.notes.concat(savedNote._id)
-    await user.save()
-    response.status(201).json(savedNote)
-})
+    user: user.id,
+  });
+  const savedNote = await note.save();
+  console.log("SAVE" + savedNote);
+  user.notes = user.notes.concat(savedNote._id);
+  await user.save();
+  response.status(201).json(savedNote);
+});
 
+notesRouter.delete("/:id", async (request, response, next) => {
+  await Note.findByIdAndDelete(request.params.id);
+  response.status(204).end();
+});
 
-notesRouter.delete('/:id', async (request, response, next) => {
-    await Note.findByIdAndDelete(request.params.id)
-      response.status(204).end()
-})
-
-notesRouter.put('/:id', (request, response, next) => {
-  const body = request.body
+notesRouter.put("/:id", (request, response, next) => {
+  const body = request.body;
 
   const note = {
     content: body.content,
     important: body.important,
-  }
-
+  };
 
   Note.findByIdAndUpdate(request.params.id, note, { new: true })
-    .then(updatedNote => {
-      response.json(updatedNote)
+    .then((updatedNote) => {
+      response.json(updatedNote);
     })
-    .catch(error => next(error))
-})
+    .catch((error) => next(error));
+});
 
-module.exports = notesRouter
+module.exports = notesRouter;
