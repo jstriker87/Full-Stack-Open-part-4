a0b39e2aac1f1fcd48c7c67ee4ce1effc995591b
James Bart <jbart1@gmail.com>
2024-11-15 20:07:44 +0000

    Completed exercise 4.17 and 4.18

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -12,7 +12,7 @@ const getTokenFrom = (request) => {
 };
 
 blogsRouter.get("/", async (request, response) => {
-  const blogs = await Blog.find({});
+  const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 });
   response.json(blogs);
 });
 
