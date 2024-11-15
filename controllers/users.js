a4981af783e40d27f3d680ccb42116de519d4e02
James Bart <jbart1@gmail.com>
2024-11-15 00:58:07 +0000

    Completed exercise 4.15. Working on the fimal part of exercise 4.16 to refactor tests

--- controllers/users.js
+++ controllers/users.js
@@ -6,6 +6,10 @@ usersRouter.post("/", async (request, response) => {
   const { username, name, password } = request.body;
 
   const saltRounds = 10;
+  if (password.length < 3) {
+    return response.status(400).json({ error: " Password length invalid" });
+  }
+
   const passwordHash = await bcrypt.hash(password, saltRounds);
 
   const user = new User({
